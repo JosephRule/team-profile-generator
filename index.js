f2ff07d8c57ef7050365e603a8aae2748e77471c
JosephRule <josephrule1@gmail.com>
2022-04-02 18:17:17 -0700

    added classes

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-//
+//const Employee = require("./lib/Employee.js")
+const Intern = require("./lib/Intern.js")
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineer.js");
+const Employee = require("./lib/Employee.js");
 
+const eng = new Engineer(name="joe", id="1", email="jrule@gmail.com", github="http://github.com/JosephRule")
+console.log(eng)
+
+
+const manager = new Manager(name = "Nikita", id="2", email="nparikh@stuff.com", officeNumber = 2)
+console.log(manager)
+
+const intern = new Intern(name = "joe", id = "2", email = "stuf@email.com", school = "stuff")
+console.log(intern)
+console.log(intern.getRole())
 // svg icons
 // <i class="bi bi-cup"></i>
 // <i class="bi bi-wrench"></i>
